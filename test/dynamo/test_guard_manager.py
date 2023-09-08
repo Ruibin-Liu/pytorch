2ea6b5e14d3f977dafe5e2d3e3396a3904baf515
Animesh Jain <anijain@umich.edu>
2023-09-07 21:43:54 -0700

    wip
    ghstack-source-id: 03a1bbada805cb5471e4fb81ee0ebfc7954b92d0
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/108839

---
+++ test/dynamo/test_guard_manager.py
@@ -0,0 +1,31 @@
+
+import torch
+
+from torch._C._dynamo import guards
+
+
+
+const_guard = guards.PythonLambdaGuard(lambda x: x == 5)
+print(const_guard(4))
+print(const_guard(5))
+
+
+guard_mananger = guards.GuardManager()
+guard_mananger.add_lambda_guard(lambda x: isinstance(x, int))
+guard_mananger.add_lambda_guard(lambda x: x >= 5)
+
+print([guard_mananger.check(i) for i in range(4, 8)])
+
+
+guard_mananger.animesh.add_lambda_guard(lambda x: isinstance(x, int))
+guard_mananger.animesh.add_lambda_guard(lambda x: x <= 10)
+guard_mananger.animesh.add_lambda_guard(lambda x: x >= 5)
+print(guard_mananger.animesh.check(9), guard_mananger.animesh.check(11))
+
+
+
+
+guard_mananger["foo"].add_lambda_guard(lambda x: isinstance(x, int))
+guard_mananger["foo"].add_lambda_guard(lambda x: x <= 10)
+guard_mananger["foo"].add_lambda_guard(lambda x: x >= 5)
+print(guard_mananger["foo"].check(9), guard_mananger["foo"].check(11))
